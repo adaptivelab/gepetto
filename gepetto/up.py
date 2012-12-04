936c22c526d8e6f72707be6d088cdb63fefbf375
Rob Berry <rob@luvhate.us>
2012-12-04 11:55:53 +0000

    Replace local config parsing with boto.config

--- gepetto/up.py
+++ gepetto/up.py
@@ -1,33 +1,7 @@
 #!/usr/bin/env python
-import ConfigParser
-import collections
 import time
-import sys
 from boto.ec2 import connect_to_region
-
-Credentials = collections.namedtuple('Credentials', ['key', 'secret'])
-
-
-class Config(object):
-
-    @property
-    def config(self):
-        if not hasattr(self, '_config'):
-            try:
-                fp = open('creds.ini')
-            except IOError as err:
-                sys.exit(err)
-            config = ConfigParser.ConfigParser()
-            config.readfp(fp)
-            self._config = config
-        return self._config
-
-    def __call__(self):
-        return self.config
-
-
-get_config = Config()
-
+from boto import config
 
 # alestic Ubuntu 12.04 LTS Precise EBS boot
 AMI_ID = 'ami-3b65664f'
@@ -36,17 +10,11 @@ AMI_ID = 'ami-3b65664f'
 # AMI_ID = 'ami-c1aaabb5'
 
 
-def credentials():
-    config = get_config()
+def connection():
     secret = config.get('credentials', 'aws_secret_access_key')
     key = config.get('credentials', 'aws_access_key_id')
-    return Credentials(key=key, secret=secret)
-
-
-def connection():
-    creds = credentials()
-    return connect_to_region('eu-west-1', aws_access_key_id=creds.key,
-                             aws_secret_access_key=creds.secret, debug=2)
+    return connect_to_region('eu-west-1', aws_access_key_id=key,
+                             aws_secret_access_key=secret, debug=2)
 
 
 def userdata():
